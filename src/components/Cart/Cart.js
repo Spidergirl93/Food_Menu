8b8fe67a95e8860c4be9c6295fabb1f5255799e1
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-07-13 12:09:43 +0200

    add seperate pretty items to the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,10 @@
+//React imports
+import { useContext } from 'react';
+
 //Component imports
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 //CSS imports
 import styles from './Cart.module.css';
@@ -7,24 +12,25 @@ import styles from './Cart.module.css';
 //Main component 
 const Cart = (props) => {
 
+    const context = useContext(CartContext);
+
+    const removeHandler = (id) => {};
+
+    const addHandler = (item) => {};
+
 
     const cartItems = <ul className={styles[`cart-items`]}>{
-        [{
-            id: 'm1',
-            name: 'Sushi',
-            amount: '1',
-            price: '50'
-        }].map((item) => (
-            <li key={item.id}>{item.name}</li>
-        ))
+        context.items.map((item) => (
+            <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onAdd={addHandler.bind(null, item)} onRemove={removeHandler.bind(null, item.id)} />))
         }</ul>;
 
+        const totalAmount = `$${context.totalAmount.toFixed(2)}`;
     return(
         <Modal>
             {cartItems}
             <div className={styles.total}>
                 <span>Total amount</span>
-                <span>100$</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles[`button--alt`]} onClick={props.onCancel}>Cancel</button>
